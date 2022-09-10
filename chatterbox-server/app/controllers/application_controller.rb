2dd30181e0c80a035c2c20b187a6c167e9117774
Allen Mordukhaev <allenmor93@gmail.com>
2022-09-10 19:09:15 -0400

    commit tree a14025604767eb6443031a94af90949e0151613f

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,8 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
-  
+  get '/messages' do
+    message = Message.order(created_at: :asc)
+    message.to_json
+  end
 end
